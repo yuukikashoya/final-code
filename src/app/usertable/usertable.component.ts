1935239e8cea74ea9b73988362309cff2affdd08
johnlloyd <yukimiranda26@gmail.com>
2023-06-20 11:31:41 +0800

    upload

--- src/app/usertable/usertable.component.ts
+++ src/app/usertable/usertable.component.ts
@@ -1,8 +1,10 @@
 
 import { Component, OnInit } from '@angular/core';
 import {  AngularFireDatabase } from '@angular/fire/compat/database';
-import { Database,} from '@angular/fire/database';
+import { Database, ref, set,} from '@angular/fire/database';
 import { Observable } from 'rxjs';
+import { formatDate } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-usertable',
@@ -10,17 +12,60 @@ import { Observable } from 'rxjs';
   styleUrls: ['./usertable.component.css']
 })
 export class UsertableComponent implements OnInit {
-  account!: Observable<any[]>;
 
+  chats!: Observable<any[]>;
   session =  sessionStorage.getItem("id");
 
 
-  constructor(public database: Database, private FireDb: AngularFireDatabase) { 
-
-    this.account = FireDb.list('/user').valueChanges();
+  constructor(public database: Database, private FireDb: AngularFireDatabase,private router:Router) { 
+    this.chats = FireDb.list('/chat/'+ 'chatroom').valueChanges();
+ 
   }
 
   ngOnInit(): void {
   }
+
+chat = ""
+currentuser=""
+
+
+
+chatbox = false
+listcaahtt = true
+
+
+chatid =1
+iid= sessionStorage.getItem('id');
+chatcount=0
+
+sendchat(value:any){
+
+if(value.chat == null || value.chat == "" || value.chat == undefined){
+alert("wala")
+}else{
+
+const date = formatDate(new Date(), 'YYYYddMMhhmmss', 'en')
+const datenow = formatDate(new Date(), ' hh:mma MMM/dd/YYYY', 'en')
+
+
+set(ref(this.database, 'chat/' + 'chatroom' + '/'+date), {
+username: this.iid,
+chat: value.chat,
+touser: this.currentuser,
+timesent: datenow,
+id:date
+
+});
+
+this.chat = ""
+
+}
+}
+logout(){
+
+sessionStorage.clear();
+this.router.navigate(['/login'])
+}
+
 
 }
